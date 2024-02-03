88367d806d4d394ac006fe79baccf3213a9cbd34
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-03 23:47:17 +0300

    Реализовал функцию, возвращающую матрицу поворота

--- camera.py
+++ camera.py
@@ -1,11 +1,9 @@
 import typing
 import numpy as np
 from numpy import linalg
+from utils import Vector, normalize
 
-Vector = np.ndarray[float]
 
-def normalize(vec: Vector):
-    return vec / linalg.norm(vec)
 
 class Camera:
     def __init__(self, look_point: Vector, position: Vector=np.array([0.0, 0.0, 0.0]), vertical_view_angle=np.pi/4.0):
@@ -14,3 +12,7 @@ class Camera:
         self.xAxis = normalize(look_point - self.pos)
         self.yAxis = normalize(np.cross(np.array([0.0, 0.0, 1.0]), self.xAxis))
         self.zAxis = normalize(np.cross(self.xAxis, self.yAxis))
+
+    def rotation_matrix_transposed(self):
+        return np.array([self.xAxis, self.yAxis, self.zAxis])
+
