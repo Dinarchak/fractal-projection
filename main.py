387d7639f65c172a9611b6346eeedaf47ee176bb
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-03 23:47:36 +0300

    эксперементы и фиксы

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import typing
 from camera import Camera
 import numpy as np
+from utils import Vector, normalize
+from shapes import Circle
 
-Vector = np.ndarray[float]
-WIDTH = 200
-HEIGHT = 150
+WIDTH = 11
+HEIGHT = 7
+
+def ray_marching(ray, shapes, point, steps=10):
+    start_point = point
+    for i in range(steps):
+        a = min(shape.dist(point) for shape in shapes) 
+        if a < 1e-9:
+            return np.linalg.norm(point - start_point)
+        point = point + ray * a
+    return None
 
 def range_into_range(v: float, range1: tuple[float, float], range2: tuple[float, float]) -> float:
     """
@@ -29,18 +39,35 @@ def square_to_square(point: Vector) -> tuple[float, float]:
     """
     global WIDTH
     global HEIGHT
-    return range_into_range(point[0], (0.0, float(WIDTH)), (- WIDTH / HEIGHT, WIDTH / HEIGHT)), range_into_range(point[1], (0.0, float(HEIGHT)), (1.0, -1.0))
+    return range_into_range(point[0], (0.0, float(WIDTH) - 1), (- WIDTH / HEIGHT, WIDTH / HEIGHT)), range_into_range(point[1], (0.0, float(HEIGHT) - 1), (1.0, -1.0))
 
-    
 
-a = Camera(np.array([23.0, 11.0, 78.0]))
+def from_camera_to_world(camera: Camera, point: Vector):
+    return camera.pos + np.matmul(point, camera.rotation_matrix_transposed())
 
-# проверка на то, что СК камеры - правая ДПСК
-# print(a.xAxis, np.linalg.norm(a.xAxis))  # модуль должен быть равен 1
-# print(a.yAxis, np.linalg.norm(a.yAxis))  # модуль должен быть равен 1
-# print(a.zAxis, np.linalg.norm(a.zAxis))  # модуль должен быть равен 1
-# print(np.dot(np.cross(a.xAxis, a.yAxis), a.zAxis))  # смешанное произведение равно 1
+camera = Camera(np.array([1.0, 0.0, 0.0]), vertical_view_angle=np.pi/6)
+circle = Circle(np.array([1.1, 0.0, 0.4]), 1)
+
+#проверка на то, что СК камеры - правая ДПСК
+# print(camera.xAxis, np.linalg.norm(camera.xAxis))  # модуль должен быть равен 1
+# print(camera.yAxis, np.linalg.norm(camera.yAxis))  # модуль должен быть равен 1
+# print(camera.zAxis, np.linalg.norm(camera.zAxis))  # модуль должен быть равен 1
+# print(np.dot(np.cross(camera.xAxis, camera.yAxis), camera.zAxis))  # смешанное произведение равно 1
 
 frame = np.zeros(shape=(HEIGHT, WIDTH))
 
+x = 1 / np.tan(camera.vert_angle)
 
+for h in range(0, HEIGHT):
+    for w in range(0, WIDTH):
+        y, z = square_to_square(np.array([w, h]))
+        ray = normalize(np.array([x, y, z]))
+        world_ray = from_camera_to_world(camera, ray) - camera.pos
+        dist = ray_marching(world_ray, [circle], camera.pos)
+        if dist is not None:
+            frame[h][w] = 1
+
+for i in frame:
+    for j in i:
+        print(j, end=' ')
+    print()
